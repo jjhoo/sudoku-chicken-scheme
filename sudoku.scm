5a0df8188a01114d9a7d8e9f5fcf4a6ac055788e
Jani Hakala <jjhakala@gmail.com>
2018-02-18 22:10:44 +0200

    Remove solved cells from initial grid

--- sudoku.scm
+++ sudoku.scm
@@ -13,6 +13,7 @@
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
+(use srfi-1)
 (require-extension srfi-121)
 
 (define (num-to-box-number num)
@@ -24,7 +25,10 @@
 (define (cell-box-calc row col)
   (cons (num-to-box-number row) (num-to-box-number col)))
 
-(define (cell-init row col n)
+(define (scand-cell-pos cell)
+  (car cell))
+
+(define (scand-init row col n)
   (cons (list row col (cell-box-calc row col)) n))
 
 (define (str-to-solved grid)
@@ -34,7 +38,7 @@
         (let* ((val (string->number (string (car tmp))))
                (nsolved (if (= val 0)
                             solved
-                            (cons (cell-init i j val) solved)))
+                            (cons (scand-init i j val) solved)))
                (ni (if (= j 9) (+ i 1) i))
                (nj (if (= j 9) 1 (+ j 1))))
           (loop ni nj nsolved (cdr tmp))))))
@@ -47,15 +51,52 @@
                       (cond ((> n 9) (loop i (+ j 1) 1))
                             ((> j 9) (loop (+ i 1) 1 1))
                             (else (begin
-                                    (yield (cell-init i j n))
+                                    (yield (scand-init i j n))
                                     (loop i j (+ n 1)))))))))))
     (generator->list g)))
+
+(define (pos-same? pos1 pos2)
+  (equal? pos1 pos2))
+
+(define (pos-same-box? pos1 pos2)
+  (equal? (third pos1) (third pos2)))
+
+(define (pos-same-col? pos1 pos2)
+  (= (second pos1) (second pos2)))
+
+(define (pos-same-row? pos1 pos2)
+  (= (first pos1) (first  pos2)))
+
+(define (scand-same-val? cell1 cell2)
+  (= (cdr cell1) (cdr cell2)))
+
+(define (pos-sees? pos1 pos2)
+  (or (pos-same-box? pos1 pos2)
+      (pos-same-col? pos1 pos2)
+      (pos-same-row? pos1 pos2)))
+
+(define (init solved)
+  (let loop ((xs solved) (cands (grid-init)))
+    (if (null? xs)
+        cands
+        (let* ((cell1 (car xs))
+               (rest (cdr xs))
+               (pos1 (scand-cell-pos cell1))
+               (pred (lambda (cell2)
+                       (let ((pos2 (scand-cell-pos cell2)))
+                         (or (pos-same? pos1 pos2) ;; cell is solved
+                             (and (pos-sees? pos1 pos2)
+                                  (scand-same-val? cell1 cell2))))))
+               (ncands (remove pred cands)))
+          (loop rest ncands)))))
 
 (define GRID "014600300050000007090840100000400800600050009007009000008016030300000010009008570")
 (print GRID)
 
 (define solved (str-to-solved GRID))
+(print (length solved))
 (print solved)
 
-(define cands (grid-init))
+(define cands (init solved))
 (print cands)
+(print (length cands))
