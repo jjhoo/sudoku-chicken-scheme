f9df3690d7b412de7ed5eb6f9ac218b1e956fd2a
Jani Hakala <jjhakala@gmail.com>
2018-02-23 08:17:39 +0200

    Implement finding hidden singles

--- sudoku.scm
+++ sudoku.scm
@@ -14,7 +14,27 @@
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 (use srfi-1)
+(require-extension matchable)
 (require-extension srfi-121)
+
+(define (make-2d-generator yfun gen1 gen2-init)
+  (make-coroutine-generator
+   (lambda (yield)
+     (let loop ()
+       (match (gen1)
+              [#!eof (yield #!eof)]
+              [i (let loop2 ((gen2 (gen2-init)))
+                   (match (gen2)
+                          [#!eof (loop)]
+                          [j (yield (yfun i j))
+                             (loop2 gen2)]))])))))
+
+(define (box-number-to-index num)
+  (match num
+    [1 (cons 1 1)] [2 (cons 1 2)] [3 (cons 1 3)]
+    [4 (cons 2 1)] [5 (cons 2 2)] [6 (cons 2 3)]
+    [7 (cons 3 1)] [8 (cons 3 2)] [9 (cons 3 3)]
+    [_ "invalid"]))
 
 (define (num-to-box-number num)
   (case num
@@ -67,13 +87,59 @@
 (define (pos-same-row? pos1 pos2)
   (= (first pos1) (first  pos2)))
 
-(define (scand-same-val? cell1 cell2)
-  (= (cdr cell1) (cdr cell2)))
-
 (define (pos-sees? pos1 pos2)
   (or (pos-same-box? pos1 pos2)
       (pos-same-col? pos1 pos2)
       (pos-same-row? pos1 pos2)))
+
+(define (scand:get-box box cands)
+  (let ((box (if (integer? box)
+                 (box-number-to-index box)
+                 (box))))
+    (filter (match-lambda [(r c b n) (equal? box b)] [_ #f]) cands)))
+
+(define (scand:get-cell pos cands)
+  (filter (match-lambda [(p . n) (equal? pos p)] [_ #f]) cands))
+
+(define (scand:get-col col cands)
+  (filter (match-lambda [((r c (a . b)) . n) (= col c)] [_ #f]) cands))
+
+(define (scand:get-row row cands)
+  (filter (match-lambda [((r c (a . b)) . n) (= row r)] [_ #f]) cands))
+
+(define (unique-positions-gen cands)
+  ;; (unique (map (lambda (cell) (first cell)) cands)))
+  (gdelete-neighbor-dups
+   (list->generator (map (lambda (cell) (first cell)) cands))))
+
+(define (numbers cands)
+  (sort (map cdr cands) <))
+
+(define (unique-numbers-gen cands)
+  (gdelete-neighbor-dups
+   (list->generator (numbers cands))))
+
+(define (cands-sort+dedupe cands)
+  (generator->list
+   (gdelete-neighbor-dups
+    (list->generator
+     (sort cands (lambda (cell1 cell2)
+                   (cond ((< (first (car cell1)) (first (car cell2))) #t)
+                         ((> (first (car cell1)) (first (car cell2))) #f)
+                         ((< (second (car cell1)) (second (car cell2))) #t)
+                         ((> (second (car cell1)) (second (car cell2))) #f)
+                         ((< (cdr cell1) (cdr cell2)) #t)
+                         ((> (cdr cell1) (cdr cell2)) #f)
+                         (else #f))))))))
+
+(define (scand-same-pos? cell1 cell2)
+  (equal? (car cell1) (car cell2)))
+
+(define (scand-same-val? cell1 cell2)
+  (= (cdr cell1) (cdr cell2)))
+
+(define (scand-pos cell)
+  (car cell))
 
 (define (init solved)
   (let loop ((xs solved) (cands (grid-init)))
@@ -90,13 +156,83 @@
                (ncands (remove pred cands)))
           (loop rest ncands)))))
 
+(define (finder pred cands)
+  (let* ((yfun (lambda (i fun) (cons i fun)))
+         (gen (make-2d-generator
+               yfun
+               (make-iota-generator 9 1)
+               (lambda ()
+                 (list->generator (list scand:get-row
+                                        scand:get-col
+                                        scand:get-box))))))
+    (let loop ((solved '())
+               (eliminated '()))
+      (match
+       (gen)
+       [#!eof (cons (cands-sort+dedupe solved) (cands-sort+dedupe eliminated))]
+       [(i . f)
+        (begin
+          ;; (print "Now i = " i ", fun is " f)
+          (match
+           (pred (f i cands))
+           [('() . '())
+            (loop solved eliminated)]
+           [(nsolved . neliminated)
+            (loop (append nsolved solved)
+                  (append neliminated eliminated)) ]
+         ;; [xxx (begin (print "wtf " xxx ) (cons solved eliminated))]
+           ))]))))
+
+;; (for (i 1 9)
+  ;;      (dolist (fun2 funs2)
+    ;;              (set 'found (append found (pred (fun2 i cands))))))
+    ;; (unique (sort found))))
+
+;; Find unsolved cells that have only one candidate left
+(define (find-singles-simple solved cands)
+  (let ((fun (lambda (cands)
+               (generator-fold
+                (lambda (pos res)
+                  (match
+                   (generator->list
+                    (gfilter (lambda (cell)
+                               (pos-same? pos (scand-pos cell)))
+                             (list->generator cands)))
+                   ['() res]
+                   [(cell) (cons '() (cons cell (cdr res)))]
+                   [_ res]))
+                (cons '() '())
+                (unique-positions-gen cands)))))
+    (finder fun cands)))
+
+(define (find-singles solved cands)
+  (let ((fun (lambda (cands)
+               (generator-fold
+                (lambda (n res)
+                  (match
+                   (generator->list
+                    (gfilter (lambda (cell) (= n (cdr cell)))
+                             (list->generator cands)))
+                   ['() res]
+                   [(cell) (cons '() (cons cell (cdr res)))]
+                   [xxx res]))
+                (cons '() '())
+                (unique-numbers-gen cands)))))
+    (finder fun cands)))
+
+(define (solver solved cands)
+  (let ((funs (list find-singles-simple
+                    find-singles)))
+    (print (find-singles solved cands))))
+
 (define GRID "014600300050000007090840100000400800600050009007009000008016030300000010009008570")
 (print GRID)
 
 (define solved (str-to-solved GRID))
-(print (length solved))
-(print solved)
+;; (print (length solved))
+;; (print solved)
 
 (define cands (init solved))
-(print cands)
-(print (length cands))
+;; (print cands)
+;; (print (length cands))
+(solver solved cands)
