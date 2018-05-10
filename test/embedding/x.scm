65478a17b5d69137bac3f179397b7c5a8807b4a4
Jani Hakala <jjhakala@gmail.com>
2018-05-10 09:34:08 +0300

    Rename function

--- test/embedding/x.scm
+++ test/embedding/x.scm
@@ -4,18 +4,19 @@
 (define-foreign-type scell_fun (function void (int int int nonnull-c-pointer)))
 
 (define-external
-  (sudoku_test (scheme-object result)
-               (scell_fun scfun)
-               (c-pointer user-data)) void
-  (let ((asdf (foreign-lambda* void ((int r) (int c) (int v)
-                                     (scell_fun scfun)
-                                     (c-pointer userdata))
-                "scfun(r, c, v, userdata);")))
-    (sudoku-scand-map
-     (lambda (row col val)
-       ;; (print "Solved " row ", " col ", " val)
-       (asdf row col val scfun user-data))
-     (car result))))
+  (sudoku_map_result (scheme-object result)
+                     (scell_fun scfun)
+                     (c-pointer user-data)) void
+                     (let ((cfun (foreign-lambda*
+                                     void ((scell_fun scfun)
+                                           (int r) (int c) (int v)
+                                           (c-pointer userdata))
+                                   "scfun(r, c, v, userdata);")))
+                       (sudoku-scand-map
+                        (lambda (row col val)
+                          ;; (print "Solved " row ", " col ", " val)
+                          (cfun scfun row col val user-data))
+                        (car result))))
 
 (define-external (sudoku_solve (c-string sudoku)) scheme-object
   (let* ((solved (sudoku-string->grid sudoku))
