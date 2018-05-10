306cdda0e087a3cf9e4d581f6b53d3d105d0d9b7
Jani Hakala <jjhakala@gmail.com>
2018-05-10 09:34:08 +0300

    Rename function

--- test/embedding/foo.c
+++ test/embedding/foo.c
@@ -2,9 +2,9 @@
 #include <chicken.h>
 
 extern C_word sudoku_solve(char * sudoku);
-extern void sudoku_test(C_word res,
-                        void  (*t1)(int, int, int, void *),
-                        void * user_data);
+extern void sudoku_map_result(C_word res,
+                              void  (*t1)(int, int, int, void *),
+                              void * user_data);
 
 void test(int row, int col, int val, void * user_data)
 {
@@ -19,7 +19,7 @@ int main()
   CHICKEN_run(C_toplevel);
 
   C_word res = sudoku_solve("000704005020010070000080002090006250600070008053200010400090000030060090200407000");
-  sudoku_test(res, test, NULL);
+  sudoku_map_result(res, test, NULL);
 
   return 0;
 }
